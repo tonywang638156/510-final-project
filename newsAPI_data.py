a5e1f2dc9ec0909ee14bcd0278a1be41e1d59319
tonywang638156 <luoli.wang@richmond.edu>
2024-11-25 12:45:21 -0500

    news api modification

--- newsAPI_data.py
+++ newsAPI_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Define your NewsAPI key
-news_api_key = ""
+news_api_key = "56a09efdc146428fbf0385e61c134591"
 
 # Base URL for NewsAPI
 news_base_url = "https://newsapi.org/v2/everything"
